abe77aab31918a63540211e97786b14f16842be2
Himanshu Raj <himanshuraj051@gmail.com>
2022-04-05 19:39:02 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const dummy = require('./dummyData').data;
+const { default: mongoose } = require('mongoose');
+const bodyParser = require("body-parser");
+const req = require('express/lib/request');
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// mongoose.connect('mongodb://localhost:27017/blog').then(() => {
+//     console.log("connect to database!");
+// })
+
+let data = dummy;
+
+//home
+app.get('/', (req, res) => {
+    res.send("<h1>Home</h1>");
+});
+
+//blog-list
+app.get('/blog', (req, res) => {
+    console.log(data);
+    res.render('index', { blog: data });
+});
+
+//new-blog-form
+app.get('/blog/new', (req, res) => {
+    res.render('createBlogForm')
+});
+
+//add-new-blog
+app.post('/blog', (req, res) => {
+    console.log(req.body);
+    data.push({ title: req.body.title, body: req.body.body });
+    res.redirect('/blog');
+});
+
+//view single-blog
+app.get('/blog/:id', (req, res) => {
+    let id = req.params.id;
+    console.log("reached here!!", id);
+    res.render('Blog')
+})
+
+const addSlug = url => {
+    const slugs = { 'A': 'a' };
+    Object.keys(slugs).map((key) => {
+        if (url.match(new RegExp(key))) {
+            // console.log(url, slug);
+            url = url.replace(key, slugs[key])
+            return;
+        };
+    })
+
+    return url;
+}
+
+console.log(addSlug('/A/fdvnjn'));
+console.log(addSlug('/B-C/vjfdn/cdkc'));
+
+app.listen(3001, () => {
+    console.log("listening on port 3001")
+})
---
+++ dummyData.js
@@ -0,0 +1,35 @@
+let data = [{
+        id: '1',
+        title: "blog1",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        id: '2',
+        title: "blog2",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        id: '3',
+        title: "blog3",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        id: '4',
+        title: "blog4",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        id: '5',
+        title: "blog5",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        id: '6',
+        title: "blog6",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+]
+
+module.exports = {
+    data
+}
